7e0f44bed8dfd2e2d00639d7c384ea74c874075d
蒋耀萱 <760450699@qq.com>
2018-09-15 17:18:51 +0800

    【增加PaymentHelper】

---
+++ CloverNet.ModelParamter/CloverNet.PaymentQueryParamter/PaymentHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloverNet.PaymentParamter
+{
+    public class PaymentHelper
+    {
+        public static string AutoSubmitForm(string url, string formParamter)
+        {
+            return $@"<form action='{url}' method='post'>{formParamter}<input type='submit'></form>
+                        <script>
+                            document.querySelector('[action = \'{url}\']').submit();
+                        </script>";
+        }
+
+    }
+}
